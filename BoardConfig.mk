351f41cf86600cf2dc73f56b13bbd5967ea275db
NopeNopeGuy <kingdutchisbad@gmail.com>
2023-11-29 21:03:32 +0530

    tulip: Add Metadata partition
    Logdump partition is pretty useless so use it for metadata because why not

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,10 @@ TARGET_SCREEN_DENSITY := 440
 # Kernel
 TARGET_KERNEL_CONFIG += tulip_defconfig
 
+# Creates metadata partition mount point under root for
+# the devices with metadata parition
+BOARD_USES_METADATA_PARTITION := true
+
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/manifest.xml
 
