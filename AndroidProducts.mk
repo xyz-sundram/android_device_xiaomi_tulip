4e5b0c84be7627431dd8904d21248120f2f4c832
xyz-sundram <sundrraam@gmail.com>
2024-02-23 21:37:34 +0530

    tulip: Voltage OS -use build fingerprint from source

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tulip.mk
+    $(LOCAL_DIR)/voltage_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_tulip-user \
-    lineage_tulip-userdebug \
-    lineage_tulip-eng
+    voltage_tulip-user \
+    voltage_tulip-userdebug \
+    voltage_tulip-eng
